d18966ecebf416477f10d0f63205500c54543f46
Maarek <Maarek.sillamae@khk.ee>
2019-09-25 11:36:30 +0300

    muutujad

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -2,6 +2,12 @@
     <div>
         <labkel for="nimi">Nimi:</labkel>
         <input  type="text" id="nimi" name="nimi">
+    </div>     <div>
+        <labkel for="pikkus">Pikkus:</labkel>
+        <input  type="number" id="pikkus" name="nimi">
+    </div>    <div>
+        <labkel for="kaal">Kaal:</labkel>
+        <input  type="number" id="kaal" name="nimi">
     </div>
     <input type="submit" value="saada">
 </form>
--- 25.09.2019/input_do.php
+++ 25.09.2019/input_do.php
@@ -1,5 +1,11 @@
 <?php
 $nimi = $_GET['nimi'];
+$pikkus = $_GET['pikkus'];
+$kaal = $_GET['kaal'];
+
 echo 'nimi:'.$nimi. '<br>';
+echo 'pikkus:'.$pikkus. '<br>';
+echo 'kaal:'.$kaal. '<br>';
+$kmi = $kaal / ($pikkus * $pikkus );
 echo'<hr>';
 echo '<a href="input.php">sisesta andmed uuesti</a>';
