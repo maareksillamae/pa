c4f5b65db44869043145dce355841a9c745efd32
Maarek <Maarek.sillamae@khk.ee>
2019-09-25 11:28:58 +0300

    dynaaminile defineerimine muutujal

---
+++ 25.09.2019/input.php
@@ -0,0 +1,7 @@
+<form action="input_do.php"method="get">
+    <div>
+        <labkel for="nimi">Nimi:</labkel>
+        <input  type="text" id="nimi" name="nimi">
+    </div>
+    <input type="submit" value="saada">
+</form>
---
+++ 25.09.2019/input_do.php
@@ -0,0 +1,5 @@
+<?php
+$nimi = $_GET['nimi'];
+echo 'nimi:'.$nimi. '<br>';
+echo'<hr>';
+echo '<a href="input.php">sisesta andmed uuesti</a>';
