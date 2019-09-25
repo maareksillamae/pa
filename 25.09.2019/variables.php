8171e7262fc44de9c74bbba4816063b015e5caf9
Maarek <Maarek.sillamae@khk.ee>
2019-09-25 11:01:01 +0300

    muutujad

---
+++ 25.09.2019/variables.php
@@ -0,0 +1,21 @@
+<?php
+// muutaja defineerimine
+// $muutujaNimi=v22rtus;
+//v''rtuseed- andmed- andmetyybid
+//tekst -sting- "info jutum'rkides"
+// t2isarvud - integer - 2 7 9 15 jne
+// komaga arvud ehk real arvud - float - 3.5 7.9 -58.45  jne
+
+$eesNimi = 'Maarek';
+$pereNimi = "Sillamäe";
+$email = 'maarek.sillamae@khk.ee';
+$vanus= 25;
+$pikkus= 1.73;
+$kaal= 73;
+
+echo 'Eesnimi:'.$eesNimi.'<br>';
+echo 'Perenimi:'.$pereNimi.'<br>';
+echo 'Email:'.$email.'<br>';
+echo 'Vanus:'.$pikkus.'<br>';
+echo 'Kaal:'.$kaal.'<br>';
+
