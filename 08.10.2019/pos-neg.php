415ad42d96ec91378080b3d4c5083de9ddde8e48
Maarek <Maarek.sillamae@khk.ee>
2019-10-08 09:05:28 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

---
+++ 08.10.2019/pos-neg.php
@@ -0,0 +1,12 @@
+<?php
+$number = -25;
+var_dump($number>0);
+echo '<br>';
+    if($number>0){
+        echo $number. ' on positiivne';
+    }else{
+        echo $number. ' on neg';
+
+    }
+
+    echo'programmi lpp';
