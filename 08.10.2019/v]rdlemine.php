2fa1e7bbf3aa4e150043bea2e8e1fdb6f6e0d7cb
Maarek <Maarek.sillamae@khk.ee>
2019-10-08 10:53:04 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

---
+++ 08.10.2019/v]rdlemine.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Declaration of strings
+$num1 = (int)"";
+$num2 = (int)"";
+
+// Use strcmp() function
+if (strcmp($num1, $num2) !== 0) {
+    echo 'Both strings are not equal';
+} else {
+    echo 'Both strings are equal';
+}
