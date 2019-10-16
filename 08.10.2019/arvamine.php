4e0f13eea9bb556eae7aca60c80ca8755c8d4018
Maarek <Maarek.sillamae@khk.ee>
2019-10-16 22:24:35 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

---
+++ 08.10.2019/arvamine.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$number= $_POST['number_entered'];
+
+$submitbutton= $_POST['submit'];
+
+
+$randomnumber= mt_rand(1,10);
+
+
+?>
+
+
+<form action="" method="POST">
+Guess a Number Between 1 and 10:
+<input type="text" name="number_entered" value=''/> <br><br>
+
+Result:
+<?php
+if ($submitbutton){
+
+if (($number > 0) && ($number <11)){
+if ($number != $randomnumber)
+{
+echo "Incorrect guess. The correct number was $randomnumber. Try again";
+}
+else
+{
+echo "$randomnumber is the correct guess. You got it right.";
+}
+}
+
+}
+
+?>
+<br><br>
+<input type="submit" name="submit" value="Search"/><br><br>
+</form>
+
---
+++ 08.10.2019/equal or bigger.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// Variable contains integer value
+$x = 993;
+
+// Vatiable contains string value
+$y = '999';
+
+// Compare $x and $y
+if ($x == $y)
+    echo 'Same content';
+else
+    echo 'Different content';
+ ?>
