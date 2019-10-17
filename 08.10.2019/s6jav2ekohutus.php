32619ccb9061aef5c2867ff60e946528ef4b0c4f
Maarek <Maarek.sillamae@khk.ee>
2019-10-17 08:48:56 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

--- 08.10.2019/s6jav2ekohutus.php
+++ 08.10.2019/s6jav2ekohutus.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 $nation = $_GET ['nation'];
@@ -22,46 +24,4 @@ if($edu !='edu')
 {
     echo'p6hiharidus';
 }
-
-
-
-?>
-
-<div id="container"
-<form action="" method="get">
-    <div id="input-group">
-        <label for="nation">kodakondsus</label>
-        <select id="nation" name="nation">
-            <option value="et">eestlane</option>
-            <option value="ru">venelane</option>
-            <option value="fi">soomlane</option>
-            <option value="se">rootslane</option>
-
-        </select>
-    </div>
-    <div id="input-group">
-        <label for="age">vanus</label>
-        <input type="number" min="0" id="age" name="age">
-    </div>
-    <div id="input-group">
-        <label for="ed"
-        </label>
-        <input type="radio" id="edu" name="ed" value="alg">
-    <div id="input-group">
-        <label for="edu">p6hikool
-        </label>
-   <input type="radio" id="edu" name="edu" value="basic">
-        <label for="edum">keskkool
-        </label>
-        <input type="radio" id="edum" name="edu" value="medium">
-        <label for="eduh">k6rgkool
-        </label>
-        <input type="radio" id="eduh" name="edu" value="high">
-
-    </div>
-
-    <div id="input-group">
-<input type="submit" value="kontrolli">
-    </div>
-</form>
 
