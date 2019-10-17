d3be3e834f1b97321c9252757ff9ba4b2fafc44b
Maarek <Maarek.sillamae@khk.ee>
2019-10-17 11:25:28 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

--- 17.10.2019/prime number.php
+++ 17.10.2019/prime number.php
@@ -1,18 +1,24 @@
 <?php
-$numer=101;
-if($number<5){
-    echo'antud arv ei kuulu valitud vahemiku sisse';
+$sum = 0;
+for ($numer=1; $number
+    <=20;$number++)
+if($number<2){
+    echo'antud arv ei kuulu valitud vahemiku sisse<br>';
 }
 else{
     $divider = 2;
-    while($number %$divider !=0) {
+    while($number % $divider !=0) {
         $divider++;
 
     }
     if ($number == $divider){
         echo $number.' on algarv<br>';
+        $sum = $sum+$number;
+
     }
     else{
         echo $number. 'ei ole alg arv<br>';
     }
 }
+echo '<hr>';
+echo 'antud summa on '.$sum;
