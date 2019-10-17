b784e84e60b38043c78697ecab1c0b322f4fb109
Maarek <Maarek.sillamae@khk.ee>
2019-10-17 09:30:15 +0300

    uuued ]pitud asjad pi lisatud ymbardamised 3 kohta

--- 17.10.2019/asteriks tabel.php
+++ 17.10.2019/asteriks tabel.php
@@ -1,12 +1,14 @@
 <?php
 
-for($row = 1; $row<=5; $row++)
+for($row = 1; $row<6; $row++)
 
 {echo $row.'&nbsp';
     if($row <10){
         echo'&nbsp;&nbsp';
     }
-   for($col = 1;$col<=$row; $col++)
+
+       // for($col = 1;$col<=$row; $col++)
+    for($col = 1;$col<=6-$row; $col++)
        echo '*';
     echo'&nbsp';
     echo'<br>';
