ff981c49dff47f1ee5edcad097916033b8563e7b
Maarek <Maarek.sillamae@khk.ee>
2019-10-21 12:21:00 +0300

    ül 7 kolmnurga c leidmine

---
+++ 21.10.2019/kolmnurk.php
@@ -0,0 +1,11 @@
+<?php
+
+$Adjacent=5;
+$Opposite=3;
+
+//Hypotenuse ehk c=
+$Hypotenuse1 = (pow($Adjacent,2)) + (pow($Opposite,2));
+$Hypotenuse1=sqrt($Hypotenuse1);
+echo "Hüpotenuse ehk c= ".$Hypotenuse1;
+
+
