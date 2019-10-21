f3764aabf98c9dbe34689044e3371b3eb9fcfbe2
Maarek <Maarek.sillamae@khk.ee>
2019-10-21 11:41:04 +0300

    ül 7 jagamine

---
+++ 21.10.2019/jagamine.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>$division</title>
+</head>
+<body>
+
+<form>
+    <div>Number 1:</div>
+    <input type="number" name="num1"/>
+    <div>Number 2:</div>
+    <input type="number" name="num2"/>
+    <div><br><input type="submit" value="CHECK THE SUM"></div><br>
+</form>
+
+<?php
+ $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+
+
+if (isset($_GET['num1']) && isset($_GET['num2']));
+
+    $division = $num1 / $num2;
+    echo $num1 . " / " . $num2 . " = " . $division;
+
+
+?>
+
+</body>
+</html>
