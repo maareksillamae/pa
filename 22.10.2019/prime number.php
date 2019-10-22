4ed88b1482adf3273adc0f518b2ecfb1d131d476
Maarek <Maarek.sillamae@khk.ee>
2019-10-22 10:27:49 +0300

    void funktsioon

---
+++ 22.10.2019/prime number.php
@@ -0,0 +1,26 @@
+<?php
+
+//define funktsion
+function isPrimeNumber($number)
+{
+    if($number<2){
+        echo'antud arv ei kuulu valitud vahemiku sisse<br>';
+    }
+    else{
+        $divider = 2;
+        while($number % $divider !=0) {
+            $divider++;
+
+        }
+        if ($number == $divider){
+            echo $number.' on algarv<br>';
+            $sum = $sum+$number;
+
+        }
+        else{
+            echo $number. 'ei ole alg arv<br>';
+        }
+    }
+}
+//use function
+isPrimeNumber(rand(0,1000));
